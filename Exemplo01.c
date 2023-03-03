ee7f4de15858cfd3b306b0d5c06f9a12f5776040
danisantosss <danisantos5075@gmail.com>
2023-03-03 02:50:34 -0300

    Introdução básica

---
+++ Exemplo01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//Biblioteca padrão de entrada e saída
+#include <stdlib.h>
+
+int main(void){
+	printf("Nome:\t Daniel Santos\n");
+	printf("Email:\t daniel@daniel.com.br\n\a");
+//	"n" pula linha
+//	"t" faz uma tabulação
+	return 0;
+}
