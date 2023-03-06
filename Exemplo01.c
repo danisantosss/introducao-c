18e19f455d97ea3c565af6227459ecce47527414
DANIEL SANTOS <daniel04.santos@catolicasc.edu.br>
2023-03-06 20:35:28 -0300

    melhorias no exercício 1

--- Exemplo01.c
+++ Exemplo01.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-//Biblioteca padrão de entrada e saída
-#include <stdlib.h>
+#include <stdio.h> //Biblioteca padrão de entrada e saída
+#include <stdlib.h> // Biblioteca padrão de bibliotecas
 
-int main(void){
+// Função principal
+int main(void){ //chamada da função 
 	printf("Nome:\t Daniel Santos\n");
-	printf("Endereco:\t Nao te interessa 935\n");
+	printf("Endereco: Nao te interessa 935\n");
 	printf("Email:\t daniel04.santos@catolicasc.edu.br\n\a");
 	printf("CPF:\t 111.111.111-11\n");
 //	"n" pula linha
